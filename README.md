078b75312fbc02c69038733bcdbf31e741e410f6
Rodrigo de Oliveira Soares <rodrigo.o.soares@hotmail.com>
2022-03-15 21:04:19 -0300

    Add swagger and READEME

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project overview
+This is a Spring project With java that implements MongoDB integration usgin Spring Data MongoDB Project.
+
+
+# Project instructions
+This is a Web API that allows CRUD operations for manipulate user information
+For more information see swagger page, that is created alongside the project:
+(http://localhost:8080/swagger-ui/index.html#/)
---
+++ src/main/java/poc/mongo/mongoapp/configurations/SwaggerConfiguration.java
@@ -0,0 +1,24 @@
+package poc.mongo.mongoapp.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("poc.mongo.mongoapp.controllers"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
